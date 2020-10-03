d1c6e286761453e6dcc428839a98c046184ca269
MarkSmagin <lifeindeadcity@gmail.com>
2020-10-03 08:38:55 +0300

    Техническое задание №19 Блоки таблица и фильмы выводятся с помощью js

--- js/films.js
+++ js/films.js
@@ -141,24 +141,11 @@ films.forEach(function(item, i){
     })
 })
 
-
-
 let filmsTableReady = setTimeout(function ready(){
     if (numberFilms == films.length){
       $(".owl-carousel").owlCarousel({
         nav:true,
         loop:true,
-          responsive:{ 
-              0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-            }
       })
     }
     else
